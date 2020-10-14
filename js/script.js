bc302e5c89ac100c9026e50383423edbe436fc81
YakovenkoND <67145488+YakovenkoND@users.noreply.github.com>
2020-10-14 14:23:03 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  $('.slider-img').slick({
+    dots: true,
+    slidesToShow: 1,
+    arrows: false,
+    infinite: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+    {
+        breakpoint: 1180,
+        settings: {
+          dots:false
+        }
+      },
+    ]
+  });
+  $('.hits-inner').slick({
+    dots: false,
+    slidesToShow: 4,
+    arrows: true,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      // {
+      //   breakpoint: 806,
+      //   settings: {
+      //     slidesToShow: 2,
+      //   }
+      // },
+      
+    ]
+  });
+});
+
